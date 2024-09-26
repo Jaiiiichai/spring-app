0d03112714871508d7dc1d6394744c98d708f262
xylerjace <jaydicdiquin@gmail.com>
2024-09-26 16:03:05 +0800

    init

---
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -0,0 +1,20 @@
+package com.example.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
+
+}
